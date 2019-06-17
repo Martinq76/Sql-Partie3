993e65d8ee0a04abd3afbb019de2e167e5f09ca8
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-17 16:44:41 +0200

    rectif partie

--- index.sql
+++ index.sql
@@ -1,23 +1,23 @@
-use languages;
+use `languages`;
 
-ALTER TABLE languages ADD COLUMN versions VARCHAR(30);
+ALTER TABLE `languages` ADD COLUMN `versions` VARCHAR(30);
 
-ALTER TABLE frameworks ADD COLUMN version INT;
+ALTER TABLE `frameworks` ADD COLUMN `version` INT;
 
-ALTER TABLE languages CHANGE versions version VARCHAR(30) NOT NULL;
+ALTER TABLE `languages` CHANGE `versions` `version` VARCHAR(30) NOT NULL;
 
-ALTER TABLE frameworks CHANGE name framework INT NOT NULL;
+ALTER TABLE `frameworks` CHANGE `name` `framework` VARCHAR(30) NOT NULL;
 
-ALTER TABLE frameworks MODIFY version VARCHAR(10);
+ALTER TABLE `frameworks` MODIFY `version` VARCHAR(10);
 
-use codex;
+use `codex`;
 
-ALTER TABLE clients DROP secondPhoneNumber;
+ALTER TABLE `clients` DROP `secondPhoneNumber`;
 
-ALTER TABLE clients CHANGE firstPhoneNumber phoneNumber INT;
+ALTER TABLE `clients` CHANGE `firstPhoneNumber` `phoneNumber` INT;
 
-ALTER TABLE clients MODIFY phoneNumber VARCHAR(30);
+ALTER TABLE `clients` MODIFY `phoneNumber` VARCHAR(30);
 
-ALTER TABLE clients ADD COLUMN zipCode VARCHAR(30);
+ALTER TABLE `clients` ADD COLUMN `zipCode` VARCHAR(30);
 
-ALTER TABLE clients ADD COLUMN city VARCHAR(30);
+ALTER TABLE `clients` ADD COLUMN `city` VARCHAR(30);
